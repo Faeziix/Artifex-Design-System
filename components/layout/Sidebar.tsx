90102164df8f49703deb78ec7c2322fd71a6cb99
Faez <faez@psight.io>
2023-09-17 20:02:25 +0400

    feat(Sidebar.tsx): add useRef hook to create a reference to the sidebar component and useClickOutside hook to close the sidebar when clicking outside of it fix(Sidebar.tsx): remove unused import of styles module refactor(Sidebar.tsx): improve sidebar animation by adding duration to the transition feat(package.json): add sass package as a dependency to enable the use of Sass in the project

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
-import styles from "./Sidebar.module.css";
 import { Squash as Hamburger } from "hamburger-react";
+import useClickOutside from "@/hooks/useClickOutside";
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef(null);
+
+  useClickOutside(ref, () => setIsOpen(false));
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
@@ -12,7 +15,6 @@ const Sidebar = () => {
 
   return (
     <>
-      {/* Background overlay */}
       {isOpen && (
         <motion.div
           className="fixed inset-0 bg-black opacity-50"
@@ -22,20 +24,22 @@ const Sidebar = () => {
         ></motion.div>
       )}
 
-      <div
-        className={`fixed inset-y-0 left-0 w-64 bg-primary transform transition-transform ease-in-out ${
-          isOpen ? "translate-x-0" : "-translate-x-full"
-        }`}
-      >
-        {/* Sidebar content goes here */}
-      </div>
+      <div ref={ref}>
+        <div
+          className={`fixed inset-y-0 left-0 w-64 bg-primary transform transition-transform ease-in-out duration-300 ${
+            isOpen ? "translate-x-0" : "-translate-x-full"
+          }`}
+        >
+          {/* Sidebar content goes here */}
+        </div>
 
-      <button
-        className="fixed top-5 left-5 p-2 bg-primary text-white rounded-md focus:outline-none"
-        onClick={toggleSidebar}
-      >
-        <Hamburger toggled={isOpen} toggle={setIsOpen} />
-      </button>
+        <button
+          className="fixed top-5 left-5 p-2 bg-primary text-white rounded-md focus:outline-none"
+          onClick={toggleSidebar}
+        >
+          <Hamburger toggled={isOpen} toggle={setIsOpen} />
+        </button>
+      </div>
     </>
   );
 };
